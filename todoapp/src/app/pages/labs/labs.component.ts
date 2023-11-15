2eefa86f6005a7e7908ed46974491105fa34941c
LAPTOP-VUQ1J6NP\Victor_Moreno <victorsahir1011@gmail.com>
2023-11-14 21:44:18 -0500

    Event Binding: click y doble click.

--- todoapp/src/app/pages/labs/labs.component.ts
+++ todoapp/src/app/pages/labs/labs.component.ts
@@ -24,4 +24,12 @@ export class LabsComponent {
       age: 18,
       avatar: 'https://w3schools.com/howto/img_avatar.png'
     };
+
+    clickHandler() {
+      alert('Hola');
+    }
+
+    changeHandler(event: Event) {
+      console.log(event);
+    }
 }
