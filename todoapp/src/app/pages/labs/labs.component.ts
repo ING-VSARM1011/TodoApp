3b90a216699a22f04a6dcdfbeaa244852bde07df
LAPTOP-VUQ1J6NP\Victor_Moreno <victorsahir1011@gmail.com>
2023-11-15 22:44:47 -0500

    Creando un Signal en Angula.

--- todoapp/src/app/pages/labs/labs.component.ts
+++ todoapp/src/app/pages/labs/labs.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SIGNAL } from '@angular/core/primitives/signals';
 
 @Component({
   selector: 'app-labs',
@@ -15,7 +16,7 @@ export class LabsComponent {
       'Crear Proyecto',
       'Crear Componentes'
     ];
-    name = 'Daniel';
+    name = signal('Daniel');
     age = 30;
     disabled = true;
     img = 'https://w3schools.com/howto/img_avatar.png';
@@ -30,7 +31,9 @@ export class LabsComponent {
     }
 
     changeHandler(event: Event) {
-      console.log(event);
+      const input = event.target as HTMLInputElement;
+      const newValue = input.value;
+      this.name.set(newValue);
     }
 
     keydownHandler(event: KeyboardEvent) {
