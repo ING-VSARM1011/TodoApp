33b65b5f60915401fcff3b3753048065d2617697
LAPTOP-VUQ1J6NP\Victor_Moreno <victorsahir1011@gmail.com>
2023-11-16 21:32:44 -0500

    Controlando un input.

--- todoapp/src/app/pages/labs/labs.component.ts
+++ todoapp/src/app/pages/labs/labs.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrls: ['./labs.component.css']
 })
@@ -49,5 +50,13 @@ export class LabsComponent {
           name: newValue
         }
       });
+    }
+
+    colorControl = new FormControl();
+
+    constructor() {
+      this.colorControl.valueChanges.subscribe(value => {
+        console.log(value);
+      })
     }
 }
